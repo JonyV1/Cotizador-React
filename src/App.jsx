58a8fe77c62e01476f2d25dd9a4a6c6da4569ee5
jonathan <Jmerol27@gmail.com>
2022-10-23 00:22:39 -0500

    agregar formato de dinero

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,74 @@
+import { useState } from 'react';
 import Header from "./components/Header"
+import Button from "./components/Button"
+import { formatearDinero } from './helpers'
 
 function App() {
 
+  const [cantidad,setcantidad] = useState(10000);
+
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
+  function handleChange (e) {
+    setcantidad(+e.target.value);
+  }
+
+  function handleClickDecremento(){
+    const valor = cantidad - STEP;
+
+    if (valor < MIN) {
+      alert('cantidad no valida');
+      return;
+    }
+
+    setcantidad(valor)
+  }
+
+  function handleClickIncremento(){
+    const valor = cantidad + STEP;
+
+    if (valor > MAX) {
+      alert('cantidad no valida');
+      return;
+    }
+
+    setcantidad(valor)
+  }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+
         <Header />
+
+        <div className='flex justify-between my-6'>
+
+          <Button 
+          
+            operador='-'
+            fn={handleClickDecremento}
+          />
+          <Button
+          
+          operador='+'
+          fn={handleClickIncremento}
+          />
+          
+        </div>
+
         <input
          type='range'
          className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+         onChange={handleChange}
+         min={MIN}
+         max={MAX}
+         step={STEP}
+         value={cantidad}
          />
+         <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
+         { formatearDinero(cantidad)}
+         </p>
     </div>
   )
 }
